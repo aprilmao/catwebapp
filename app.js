37f508142569ff77912ef19ae96a042a6c67f1c7
aprilmao <aprilmao129@gmail.com>
2018-01-26 09:32:06 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+//var express = require("express");
+//var login = require('./routes/loginroutes');
+//var bodyParser = require('body-parser');
+
+// Creating mySQL connection
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'cat-db.ch7wckwhcyug.us-west-1.rds.amazonaws.com',
+  user     : 'aprilmao129',
+  password : 'bry9212013',
+  port     : '3306',
+  database : 'ebdb'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+// Register users
+exports.register = function(req,res){
+  // console.log("req",req.body);
+  var today = new Date();
+  var users={
+    "first_name":req.body.first_name,
+    "last_name":req.body.last_name,
+    "email":req.body.email,
+    "password":req.body.password,
+    "created":today,
+    "modified":today
+  }
+  connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+  if (error) {
+    console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    console.log('The solution is: ', results);
+    res.send({
+      "code":200,
+      "success":"user registered sucessfully"
+        });
+  }
+  });
+}
+
+// Check if email exists in database
+exports.login = function(req,res){
+  var email= req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+  if (error) {
+    // console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    // console.log('The solution is: ', results);
+    if(results.length >0){
+      if([0].password == password){
+        res.send({
+          "code":200,
+          "success":"login sucessfull"
+            });
+      }
+      else{
+        res.send({
+          "code":204,
+          "success":"Email and password does not match"
+            });
+      }
+    }
+    else{
+      res.send({
+        "code":204,
+        "success":"Email does not exits"
+          });
+    }
+  }
+  });
+}
+
+
+// Creating server connection
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            // mysql connection ends
+            connection.end();
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
+
